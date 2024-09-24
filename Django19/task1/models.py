46d1f5f163f5915212697de0b484000bc4aab2a0
Alex_Black <tchernijalex@yandex.ru>
2024-09-24 21:00:02 +0200

    task_2

--- Django19/task1/models.py
+++ Django19/task1/models.py
@@ -19,3 +19,26 @@ class Game(models.Model):
 
     def __str__(self):
         return self.title
+
+'''
+QUERYSET
+
+python manage.py shell
+>>> from task1.models import Buyer
+>>> Buyer.objects.create(name='Garry', balance=111.11, age=12)
+>>> Buyer.objects.create(name='Alex', balance=5690.1, age=19)
+>>> Buyer.objects.create(name='Tonny', balance=3010.9, age=33)
+
+>>> from task1.models import Game
+>>> Game.objects.create(title='GTA5', cost=3000, size=112, description='rob and kill', age_limited=True)
+>>> Game.objects.create(title='PayDay2', cost=1500, size=44, description='rob and rob', age_limited=True)
+>>> Game.objects.create(title='Kindergarten', cost=100, size=19, description='peace bro', age_limited=False)
+
+>>> garry = Buyer.objects.get(id=1)
+>>> alex = Buyer.objects.get(id=2) 
+>>> tonny = Buyer.objects.get(id=3) 
+
+>>> Game.objects.get(id=1).buyer.set((alex, tonny))               
+>>> Game.objects.get(id=2).buyer.set((alex, tonny))               
+>>> Game.objects.get(id=3).buyer.set((alex, garry))
+'''
